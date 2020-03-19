4f94a863646181d0c22d3b47c57469c4a4d8637e
Lin Shadow <lin_shadow@mail.ru>
2020-03-19 14:15:58 -0400

    Create How many seconds since today.js

---
+++ How many seconds since today.js
@@ -0,0 +1,7 @@
+// (MEH :/)
+function getSecondsToday() {
+    let now = new Date();
+    let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    return Math.round((now - today)/1000);
+}
+console.log(getSecondsToday());
