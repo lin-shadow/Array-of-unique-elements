2de9192eeb21121969a4713e10630c8c964fe7e6
Lin Shadow <lin_shadow@mail.ru>
2020-03-19 14:19:53 -0400

    Create How many seconds till tomorrow.js

---
+++ How many seconds till tomorrow.js
@@ -0,0 +1,7 @@
+// (THAT WAS EASY AFTER THE PREVIOUS ONE)
+function getSecondsToTomorrow() {
+    let now = new Date();
+    let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+    return Math.round((tomorrow - now)/1000);
+}
+console.log(getSecondsToTomorrow());
