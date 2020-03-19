6ac733f557bb8610b88934e4102e4111dca48d70
Lin Shadow <lin_shadow@mail.ru>
2020-03-19 14:12:49 -0400

    Create Max of salaries.js

---
+++ Max of salaries.js
@@ -0,0 +1,17 @@
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+  };
+  function topSalary(obj) {
+      let max = 0;
+      let maxName = null;
+    for (let [name, salary] of Object.entries(obj)) {
+      if (max < salary) {
+          max = salary;
+          maxName = name;
+      }
+    }
+    return maxName;
+  }
+  console.log(topSalary(salaries));
