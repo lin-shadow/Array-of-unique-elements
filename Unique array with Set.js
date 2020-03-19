6008b0c7ae21ba02f1703c2d96917b89548195ea
Lin Shadow <lin_shadow@mail.ru>
2020-03-19 14:10:20 -0400

    Create Unique array with Set.js

---
+++ Unique array with Set.js
@@ -0,0 +1,6 @@
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+function unique(array) {
+    return Array.from(new Set(arr));
+  }
+  console.log( unique(values) );
